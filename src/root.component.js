1a71a29fc6cec65f43168a4a5fb46c80a8afbe5c
Adam Stankiewicz <agstanki@gmail.com>
2020-07-16 07:22:04 -0400

    initial commit

---
+++ src/root.component.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { navigateToUrl } from "single-spa";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function Root(props) {
+  return (
+    <Container className="pt-4">
+      <Row>
+        <Col>
+          <h1 className="h2">Course</h1>
+          <p className="lead">{props.name} is mounted!</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <a
+            href="/enterprise"
+            className="btn btn-primary px-3"
+            onClick={navigateToUrl}
+          >
+            Back to Dashboard
+          </a>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
